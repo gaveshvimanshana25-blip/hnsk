83969aa3784ed488722cdc3ef491bcd922bb8ff0
gaveshvimanshana25-blip <gaveshvimanshana25@gmail.com>
2026-03-13 15:45:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,20 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "oFE2BZZB#NOdhQC9AI2_l56tXJvkSDeKf7IJG547RGoguE8qkV_I",
 
+SESSION_ID: process.env.SESSION_ID || "PUT_YOUR_SESSION_ID",
+
+// fixed image link
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Hnsk23/hnsk/main/images/HANSA%20md.png",
 
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*  © by HANSAKA NIRMAN 💙",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello 👋 HANSA-MD Is Alive Now 😍*\n© by HANSAKA NIRMAN 💙",
 
-BOT_OWNER: '94789706579',
+BOT_OWNER: process.env.BOT_OWNER || "94789706579",
+
+AUTO_READ: process.env.AUTO_READ || "true",
+
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 
 };
