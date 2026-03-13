8bf7e4ac35987d4eddc0fedd27bfb5bea9b12c2f
gaveshvimanshana25-blip <gaveshvimanshana25@gmail.com>
2026-03-13 15:29:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "nIUUwBAI#RWw03a9IkLEUSRJ34ECszy8Ywne7m2ZPIf_MY62FRCE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Hnsk23/hnsk/blob/main/images/HANSA%20md.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*            © by HANSAKA NIRMAN 💙",
-BOT_OWNER: '94768546691',  // Replace with the owner's phone number
+BOT_OWNER: '94789706579',  // Replace with the owner's phone number
 
 
 };
