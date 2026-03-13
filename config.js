6a09e0bf16e61af6bf505e5b4c2f254c000231be
gaveshvimanshana25-blip <gaveshvimanshana25@gmail.com>
2026-03-13 15:35:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "nIUUwBAI#RWw03a9IkLEUSRJ34ECszy8Ywne7m2ZPIf_MY62FRCE",
+SESSION_ID: process.env.SESSION_ID || "oFE2BZZB#NOdhQC9AI2_l56tXJvkSDeKf7IJG547RGoguE8qkV_I",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Hnsk23/hnsk/blob/main/images/HANSA%20md.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*            © by HANSAKA NIRMAN 💙",
 BOT_OWNER: '94789706579',  // Replace with the owner's phone number
