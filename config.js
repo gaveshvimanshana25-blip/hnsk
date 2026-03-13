0c9ab9934683ce220dc5378012c6945fb1afc8af
gaveshvimanshana25-blip <gaveshvimanshana25@gmail.com>
2026-03-13 15:43:02 +0530

    Update image URL and reformat config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "oFE2BZZB#NOdhQC9AI2_l56tXJvkSDeKf7IJG547RGoguE8qkV_I",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Hnsk23/hnsk/blob/main/images/HANSA%20md.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*            © by HANSAKA NIRMAN 💙",
-BOT_OWNER: '94789706579',  // Replace with the owner's phone number
 
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Hnsk23/hnsk/main/images/HANSA%20md.png",
+
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 HANSA-MD Is Alive Now😍*  © by HANSAKA NIRMAN 💙",
+
+BOT_OWNER: '94789706579',
 
 };
- 
- 
- 
